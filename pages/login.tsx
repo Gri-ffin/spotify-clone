055d71bf76cc6c387a59c3ef7e29771fd07aa527
Gri-ffin <gameshunter359@gmail.com>
2022-03-10 23:32:27 +0100

    Add nice hover effects

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,13 +14,13 @@ const Login: NextPage<Props> = ({ providers }) => {
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
-            className="ml-7 rounded-full bg-[#18D860] p-5 text-white"
+            className="ml-7 rounded-full bg-[#18D860] p-5 text-white hover:bg-green-600"
             onClick={() => signIn(provider.id, { callbackUrl: '/' })}
           >
             Login with {provider.name}
           </button>
           <p className="m-3 text-center text-3xl font-bold text-white">Or</p>
-          <a className="ml-3 rounded-full bg-gray-400 p-3 text-white">
+          <a className="ml-3 rounded-full bg-gray-400 p-3 text-white hover:cursor-pointer hover:bg-gray-500">
             Check out the source code
           </a>
         </div>
