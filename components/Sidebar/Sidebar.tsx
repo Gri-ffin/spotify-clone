cca39f30d63a7523daf15c6a2afe27d2d400a058
Gri-ffin <gameshunter359@gmail.com>
2022-03-15 17:30:52 +0100

    Hide the scrollbar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -13,7 +13,7 @@ import { signOut, useSession } from 'next-auth/react'
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500">
+    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500 scrollbar-hide">
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
