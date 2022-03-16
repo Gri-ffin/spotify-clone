c560e5704bdcfc47302d08613367e7d3022e9056
Gri-ffin <gameshunter359@gmail.com>
2022-03-16 18:57:39 +0100

    add another atom for the playlist

--- hooks/useFetchFromSpotify.ts
+++ hooks/useFetchFromSpotify.ts
@@ -1,11 +1,6 @@
 import { signIn, useSession } from 'next-auth/react'
 import { useEffect } from 'react'
-import SpotifyWebApi from 'spotify-web-api-node'
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-})
+import spotifyApi from '../lib/spotify'
 
 const useFetchFromSpotify = () => {
   const { data: session } = useSession()
