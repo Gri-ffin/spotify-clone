a750bc36bc1568f43cae6e1522a402388e3a8d38
Gri-ffin <gameshunter359@gmail.com>
2022-03-16 19:28:19 +0100

    fetch the session on server before hitting the client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Center from '../components/Center/Index'
 import Sidebar from '../components/Sidebar/Sidebar'
@@ -28,3 +29,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
