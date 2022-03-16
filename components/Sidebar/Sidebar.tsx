1ab759e87a8a65c374027498fd1d9c78677d5fe3
Gri-ffin <gameshunter359@gmail.com>
2022-03-16 19:16:00 +0100

    improve sidebar styling

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -29,7 +29,7 @@ const Sidebar: React.FC = () => {
   }, [session, spotifyApi])
 
   return (
-    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500 scrollbar-hide">
+    <div className="hidden h-screen overflow-y-scroll border-r border-gray-900 p-5 text-xs text-gray-500 scrollbar-hide sm:max-w-[12rem] md:inline-flex lg:max-w-[15rem] lg:text-sm">
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
