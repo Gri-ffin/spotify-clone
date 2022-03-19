a28baee1ec678a3fd8383a3fda74e261603d57b7
Gri-ffin <gameshunter359@gmail.com>
2022-03-19 11:37:40 +0100

    remove redundant scope

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -4,7 +4,6 @@ const scopes = [
   'user-read-email',
   'playlist-read-private',
   'playlist-read-collaborative',
-  'user-read-email',
   'streaming',
   'user-read-private',
   'user-library-read',
